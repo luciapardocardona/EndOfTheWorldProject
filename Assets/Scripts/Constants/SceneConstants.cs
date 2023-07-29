e3f73bda73956202d35aa9131d8668c40f80c42f
luciapardocardona <luciapardocardona@gmail.com>
2023-07-29 18:12:01 +0200

    Added GameManager

---
+++ Assets/Scripts/Constants/SceneConstants.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneConstants
+{
+    public const string Level1 = "Level1";
+    public const string Level2 = "Level2";
+}
