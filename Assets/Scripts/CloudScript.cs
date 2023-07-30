63ce6b10d3be912936526f8c9e216a98b1b64cb7
KuroNeko-98 <110256022+KuroNeko-98@users.noreply.github.com>
2023-07-30 03:05:17 +0200

    le sol

--- Assets/Scripts/CloudScript.cs
+++ Assets/Scripts/CloudScript.cs
@@ -8,6 +8,6 @@ public class CloudScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, 0.5f * Time.deltaTime, 0));
+        transform.Translate(new Vector3(0, 1.5f * Time.deltaTime, 0));
     }
 }
